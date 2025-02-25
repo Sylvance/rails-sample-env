d5ee8b1668e5f17a8d339c77977efee9d069a455
omusamurai <9350722+Sylvance@users.noreply.github.com>
2025-02-26 00:55:51 +0300

    [Task 1] Add scaffold generator

--- lib/generators/haml/scaffold/scaffold_generator.rb
+++ lib/generators/haml/scaffold/scaffold_generator.rb
@@ -11,18 +11,24 @@ module Haml
           template "#{view}.html.haml", File.join("app/views", controller_file_path, filename)
         end
 
+        # Generate the partial templates
+        %w(_form).each do |view|
+          template "#{view}.html.haml", File.join("app/views", controller_file_path, "#{view}.html.haml")
+        end
+
         # Generate the modal templates
-        modal_views = %w(_destroy_modal _edit_modal _form_modal _index _show _show_modal)
-        modal_views.each do |view|
+        %w(_destroy_modal _edit_modal _form_modal _index _show _show_modal).each do |view|
           template "#{view}.html.haml", File.join("app/views", controller_file_path, "#{view}.html.haml")
         end
       end
 
+      hook_for :form_builder, as: :scaffold
+
       def available_views
-        %w(index edit show new _form)
+        %w(index edit show new)
       end
 
-      private
+      protected
 
       def handler
         :haml
