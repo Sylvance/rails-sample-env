84ac7d0b68563c99330758e1cdc7a99439d41f88
omusamurai <9350722+Sylvance@users.noreply.github.com>
2025-02-28 14:19:32 +0300

    [Task 9] Ensure seeds are done in migrations

--- spec/factories/vat_rates.rb
+++ spec/factories/vat_rates.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: vat_rates
+#
+#  id          :integer          not null, primary key
+#  active_from :date
+#  active_to   :date
+#  rate        :decimal(, )
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  item_id     :integer          not null
+#
+# Indexes
+#
+#  index_vat_rates_on_item_id  (item_id)
+#
+# Foreign Keys
+#
+#  item_id  (item_id => items.id)
+#
 FactoryBot.define do
   factory :vat_rate do
     rate { 20 }
